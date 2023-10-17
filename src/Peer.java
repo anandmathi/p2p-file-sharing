df5b5c2ec4c4d3755cab5c60ad6881db67b5ca37
anandmathi <anandmathi@gmail.com>
2023-10-16 20:39:20 -0400

    initial commit

---
+++ src/Peer.java
@@ -0,0 +1,9 @@
+public class Peer {
+    // initialize peer data
+    private int peerId;
+    // bitfield, ip, etc
+
+    public Peer(int peerId) {
+        this.peerId = peerId;
+    }
+}
---
+++ src/peerProcess.java
@@ -0,0 +1,21 @@
+public class peerProcess {
+    public static void main(String[] args) throws Exception {
+        // input validation: ensure argument is a valid integer & length == 1
+        if (args.length != 1) {
+            throw new Exception("Error: Invalid input. This program takes in one argument.\nUsage: java peerProcess <peerId>");
+        }
+        int peerId;
+        try {
+            peerId = Integer.parseInt(args[0]);
+        }
+        catch (Exception e) {
+            throw new Exception("Error: Invalid input. The peer ID must be an integer.\nUsage: java peerProcess <peerId>");
+        }
+
+        // create Peer...
+        // wrap this in try-catch block to verify environment setup later on
+        Peer peer = new Peer(peerId);
+
+        // run Client...
+    }
+}
