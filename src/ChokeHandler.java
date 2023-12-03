c0665ca9ba66508f46ae056b2f60a04f3f9aa785
Korygauger <kory.gauger@gmail.com>
2023-12-02 22:46:48 -0500

    optimistic unchoke

--- src/ChokeHandler.java
+++ src/ChokeHandler.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 
 // handler for threading
 public class ChokeHandler extends Thread {
@@ -32,6 +30,10 @@ public class ChokeHandler extends Thread {
         }
     }
 
+    public int getOptUnchokeID() {
+        return optUnchokeID;
+    }
+
     public ChokeHandler(int peerId, Client client, int prefNeigh) {
         this.peerId = peerId;
 
@@ -59,12 +61,37 @@ public class ChokeHandler extends Thread {
             unchoked.add(idown.get(i).getId());
         }
 
+        // still need to add a check for who is already unchoked but it might be more simple to just mass send messages
+        // also need to check to see if optamistic choke is in the arraylist and if it is we need to add another value if it isn't add the optamistic unchoke in
+
         client.updateUnchokedList(unchoked);
 
         Log.logChangePrefNeighbors(unchoked);
     }
 
-    public void optamisticUnChoke(){
+    public Integer optamisticUnChoke(Client client){
+        Map<Integer, Peer> peerMap = client.getPeermap();
+        ArrayList<Integer> unchoked = client.getUnchokedList();
+        Set<Integer> peerIds = new HashSet<>();
+        for (Map.Entry<Integer, Peer> entry : peerMap.entrySet()) {
+            peerIds.add(entry.getKey());
+        }
+
+        for (int i = 0; i < unchoked.size(); i++) {
+            peerIds.remove(unchoked.get(i));
+        }
+
+        int size = peerIds.size();
+        int item = new Random().nextInt(size); // In real life, the Random object should be rather more shared than this
+        int i = 0;
+        for(Integer in : peerIds)
+        {
+            if (i == item)
+                optUnchokeID = in;
+            i++;
+        }
+
+        return optUnchokeID;
 
     }
 
